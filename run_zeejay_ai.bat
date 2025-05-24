7cf483a170fccfe5585ad1a4cf07130e14e32e98
Zohaib Javed <159166949+zohaib-javd@users.noreply.github.com>
2025-05-24 14:59:51 +0500

    Delete run_zeejay_ai.bat

--- run_zeejay_ai.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call conda activate zeejay_agents
-cd /d "C:\Users\Zohaib\Documents\ZeejayAI"
-streamlit run app.py
-if %errorlevel% neq 0 (
-    echo An error occurred while running the Streamlit app.
-    exit /b %errorlevel%
-)
